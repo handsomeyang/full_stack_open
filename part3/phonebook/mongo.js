7d7f1012440f0d1b8cc7d9788407b38c1c1460ec
Jun Yang <handsomeyang@gmail.com>
2026-01-03 21:23:11 +1100

    Complete part3 exercise 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+if (process.argv.length === 4) {
+    console.log('give number as argument')
+    process.exit(1)
+}
+
+if (process.argv.length > 5) {
+    console.log('too many arguments')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://handsomeyang_db_user:${password}@cluster0.uyjwkv7.mongodb.net/phonebookApp?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+
+    person.save().then(result => {
+        console.log(`added ${person.name} ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
