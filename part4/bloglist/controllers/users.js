1455498452a41b3c4f5c8b25e68b0531e7531814
Jun Yang <handsomeyang@gmail.com>
2026-01-06 22:55:26 +1100

    Complete part4 exercise 4.16

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -10,6 +10,14 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (!password) {
+    response.status(400).json({ error: 'password missing' })
+  }
+
+  if (password.length < 3) {
+    response.status(400).json({ error: 'password needs to be at least 3 characters' })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
