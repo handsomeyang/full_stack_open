baee65e31e7ac98b4393c9dcc6ea52e57f48df00
Jun Yang <handsomeyang@gmail.com>
2026-01-08 16:14:42 +1100

    Complete part5 exercise 5.15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -3,8 +3,8 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  beforeEach(() => {
-    const blog = {
+  beforeEach((context) => {
+    context.blog = {
       title: 'Component testing is done with react-testing-library',
       author: 'John Doe',
       url: 'https://somewhere.org',
@@ -16,29 +16,29 @@ describe('<Blog />', () => {
       }
     }
 
-    const user = {
+    context.user = {
       token: '123456',
       username: 'John',
       name: 'John',
     }
 
-    const bumpLikes = vi.fn()
-    const removeBlog = vi.fn()
+    context.bumpLikes = vi.fn()
+    context.removeBlog = vi.fn()
 
-    render(<Blog blog={blog} user={user} bumpLikes={bumpLikes} removeBlog={removeBlog}/>)
+    render(<Blog blog={context.blog} user={context.user} bumpLikes={context.bumpLikes} removeBlog={context.removeBlog}/>)
   })
 
   test('renders title and author without url and likes by default', () => {
-    const titleElement = screen.getByText('Component testing is done with react-testing-library', {exact: false})
+    const titleElement = screen.getByText('Component testing is done with react-testing-library', { exact: false })
     expect(titleElement).toBeVisible()
 
-    const authorElement = screen.getByText('John Doe', {exact: false})
+    const authorElement = screen.getByText('John Doe', { exact: false })
     expect(authorElement).toBeVisible()
 
-    const urlElement = screen.getByText('https://somewhere.org', {exact: false})
+    const urlElement = screen.getByText('https://somewhere.org', { exact: false })
     expect(urlElement).not.toBeVisible()
 
-    const likesElement = screen.getByText('likes 0', {exact: false})
+    const likesElement = screen.getByText('likes 0', { exact: false })
     expect(likesElement).not.toBeVisible()
   })
 
@@ -47,29 +47,19 @@ describe('<Blog />', () => {
     const button = screen.getByText('view')
     await user.click(button)
 
-    const urlElement = screen.getByText('https://somewhere.org', {exact: false})
+    const urlElement = screen.getByText('https://somewhere.org', { exact: false })
     expect(urlElement).toBeVisible()
 
-    const likesElement = screen.getByText('likes 0', {exact: false})
+    const likesElement = screen.getByText('likes 0', { exact: false })
     expect(likesElement).toBeVisible()
   })
 
-// test('clicking the button calls event handler once', async () => {
-//   const note = {
-//     content: 'Component testing is done with react-testing-library',
-//     important: true
-//   }
-//
-//   const mockHandler = vi.fn()
-//
-//   render(
-//     <Blog note={note} toggleImportance={mockHandler} />
-//   )
-//
-//   const user = userEvent.setup()
-//   const button = screen.getByText('make not important')
-//   await user.click(button)
-//
-//   expect(mockHandler.mock.calls).toHaveLength(1)
-// })
+  test('bumpLikes is called twice if the like button is clicked twice', async ({ blog, user, bumpLikes, removeBlog }) => {
+    const userSetup = userEvent.setup()
+    const button = screen.getByText('like')
+    await userSetup.click(button)
+    await userSetup.click(button)
+
+    expect(bumpLikes.mock.calls).toHaveLength(2)
+  })
 })
